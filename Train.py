59f293238f64159cd9e7f8ad5db31383a852b1b6
Adishree Gupta <117741513+1stsimplifycode@users.noreply.github.com>
2025-04-21 06:02:24 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -5,12 +5,9 @@ import random
 import sys
 
 TOP_CITIES = {
-    "Mumbai": [19.0760, 72.8777],
     "Delhi": [28.7041, 77.1025],
     "Bangalore": [12.9716, 77.5946],
     "Hyderabad": [17.3850, 78.4867],
-    "Chennai": [13.0827, 80.2707],
-    "Kolkata": [22.5726, 88.3639],
     "Ahmedabad": [23.0225, 72.5714],
     "Pune": [18.5204, 73.8567],
     "Jaipur": [26.9124, 75.7873],
